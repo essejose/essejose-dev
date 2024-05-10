ce801b1e2b3a9985f0fdeb94b13571d4bb31acde
Jose Freire <ze_pk2@hotmail.com>
2024-05-10 12:38:46 -0300

    feat: simple datalayer send

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,25 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  const sendToGoogleAnalytics = () => {
+    (window as any).dataLayer = (window as any).dataLayer || [];
+    (window as any).dataLayer.push({
+      event: "event",
+      eventCategory: "click",
+      eventAction: "click",
+      eventLabel: "Logo",
+      eventValue: 1,
+    });
+  }
+
+
   return (
     <main className={styles.main}>
       <div className={styles.center}>
         <div className="container">
           <div
+            onClick={sendToGoogleAnalytics}
             className="stack"
             style={{ "--stacks": 3 } as React.CSSProperties}
           >
